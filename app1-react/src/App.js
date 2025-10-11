4e15d808ce67703480e93e0a9c6259e13420c35e
Suba <subashininatraj99@gmail.com>
2025-10-11 22:58:54 +0530

    childToParent

--- app1-react/src/App.js
+++ app1-react/src/App.js
@@ -5,16 +5,33 @@ import Counter from "./Components/Counter";
 import Todo from "./Components/Todo";
 import FetchApi from "./Components/fetchApi";
 import FetchApiTable from "./Components/fetchapi-table";
+import { useState, useRef  } from "react";
+import ChildComponent from "./Components/child-component";
+import ChildComponentUseRef from "./Components/child-comp-useref";
+
 function App() {
+  const [message, setMessage] = useState("");
+  const handleDataFromChild = (data) => {
+    setMessage(data);
+  };
+  const childRef = useRef();
+  const getChildData = () => {
+    alert(childRef.current); // Access data from child
+  };
+  
   return (
     <div className="App">
       <h1>React Coding Example</h1>
       {/* <OnclickExample />
       <FormSubmission />
-      <Counter /> */}
-      {/* <Todo /> */}
-      {/* <FetchApi /> */}
-      <FetchApiTable/>
+      <Counter />
+      <Todo />
+      <FetchApi />
+      <FetchApiTable/> */}
+      <ChildComponent sendDataToParent={handleDataFromChild} />
+      <p>Message: {message}</p>
+      <ChildComponentUseRef dataRef={childRef} />
+      <button onClick={getChildData}>Get Data</button>
     </div>
   );
 }
