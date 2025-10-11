986c5f99a624b6eec07447cacda04e871f835ce8
Suba <subashininatraj99@gmail.com>
2025-10-11 11:40:33 +0530

    Initial commit

---
+++ app1-react/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import FormDetails from "./Components/FormDetails"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>/
+      </header> */}
+      <h1>Welcome to React Coding</h1>
+      <FormDetails/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app1-react/src/Components/FormDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormDetails =()=>{
+    return (
+        <div>
+            <label>Enter your name: </label>
+            <input/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default FormDetails;
